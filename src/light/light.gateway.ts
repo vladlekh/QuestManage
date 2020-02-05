4d48e3ec057d5694d39045c9e539eb3866cc1fda
Vladislav Lekh <vladislav.lekh@gmail.com>
2020-02-05 23:50:57 +0300

    Add led light control, change commands

--- src/light/light.gateway.ts
+++ src/light/light.gateway.ts
@@ -7,6 +7,7 @@ import Readline = SerialPort.parsers.Readline;
 @WebSocketGateway(1081, { namespace: 'light' })
 export class LightGateway {
   port: Port;
+  port2: Port;
   parser: Readline;
 
   @WebSocketServer()
@@ -14,28 +15,36 @@ export class LightGateway {
 
   constructor() {
     this.parser = new Readline({ delimiter: '\n' });
-    this.port = new Port('/dev/ttyUSB11', {
+    this.port = new Port('COM13', {
+      baudRate: 9600,
+      autoReconnect: true,
+    });
+    this.port2 = new Port('COM4', {
       baudRate: 9600,
       autoReconnect: true,
     });
     this.port.pipe(this.parser);
+    this.port2.pipe(this.parser);
   }
 
   @SubscribeMessage('turn.light')
   async handleTurnLight() {
     console.log('TURN LIGHT');
     await this.port.writeCmd('startQuest');
+    await this.port2.writeCmd('startQuest');
   }
 
   @SubscribeMessage('switch.light')
   async handleSwitchLight() {
     console.log('SWITCH LIGHT');
     await this.port.writeCmd('switchLight');
+    await this.port2.writeCmd('switchLight');
   }
 
   @SubscribeMessage('reset')
   async handleResetLight() {
     console.log('RESET LIGHT');
     await this.port.writeCmd('reset');
+    await this.port2.writeCmd('reset');
   }
 }
